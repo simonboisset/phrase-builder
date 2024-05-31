fa7ec6bb4714fe3f6efc24cb58b0b2bfd5403ae4
Simon Boisset <simon@lezo.dev>
2024-05-31 23:48:37 +0200

    v0.1.1

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from 'vitest';
-import { buildPhrase } from '.';
+import { buildDobbleBracketsI18nPhrase, buildI18nPhrase, buildPhrase, fixI18nVariables } from '.';
 
 test('Phrase without parameters', () => {
   const phrase = 'Hello World';
@@ -47,4 +47,42 @@ test('Phrase with number parameters', () => {
   const phrase = 'Hello ${name} ${age} !';
   const buildedPhrase = buildPhrase(phrase, { name: 'World', age: 30 });
   expect(buildedPhrase).toBe('Hello World 30 !');
+});
+
+test('Fix i18n variables', () => {
+  const data = {
+    hello: 'Hello {{name}}',
+    goodbye: 'Goodbye {{name}}',
+  };
+  const fixedData = fixI18nVariables(data);
+  expect(fixedData).toEqual({
+    hello: 'Hello %{name}',
+    goodbye: 'Goodbye %{name}',
+  });
+  const helloSimon = buildI18nPhrase(fixedData.hello, { name: 'Simon' });
+  expect(helloSimon).toBe('Hello Simon');
+});
+
+test('i18n phrase without parameters', () => {
+  const phrase = 'Hello %{name}';
+  const buildedPhrase = buildI18nPhrase(phrase);
+  expect(buildedPhrase).toBe('Hello %{name}');
+});
+
+test('i18n phrase with parameters', () => {
+  const phrase = 'Hello %{name}';
+  const buildedPhrase = buildI18nPhrase(phrase, { name: 'World' });
+  expect(buildedPhrase).toBe('Hello World');
+});
+
+test('i18n dobble brackets phrase with parameters', () => {
+  const phrase = 'Hello {{name}}';
+  const buildedPhrase = buildDobbleBracketsI18nPhrase(phrase, { name: 'World' });
+  expect(buildedPhrase).toBe('Hello World');
+});
+
+test('i18n phrase with missing parameters', () => {
+  const phrase = 'Hello %{name}';
+  const buildedPhrase = buildI18nPhrase(phrase);
+  expect(buildedPhrase).toBe('Hello %{name}');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,24 @@
+// Replace ${variable} with value
 export const buildPhrase = (phrase: string, variables?: Record<string, string | number>) => {
   if (!variables) return phrase;
   return phrase.replace(/\${(\w+)}/g, (_, key) => variables[key]?.toString() || `\${${key}}`);
+};
+
+// Replace {{variable}} with %{variable}
+export const fixI18nVariables = (data: any) => {
+  const stringData = JSON.stringify(data);
+  const fixedData = stringData.replace(/{{(\w+)}}/g, '%{$1}');
+  return JSON.parse(fixedData);
+};
+
+// Replace %{variable} with value
+export const buildI18nPhrase = (phrase: string, variables?: Record<string, string | number>) => {
+  if (!variables) return phrase;
+  return phrase.replace(/%{(\w+)}/g, (_, key) => variables[key]?.toString() || `%{${key}}`);
+};
+
+// Replace {{variable}} with value
+export const buildDobbleBracketsI18nPhrase = (phrase: string, variables?: Record<string, string | number>) => {
+  if (!variables) return phrase;
+  return phrase.replace(/{{(\w+)}}/g, (_, key) => variables[key]?.toString() || `{{${key}}}`);
 };
